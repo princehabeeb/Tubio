020e1d62cc916fd5dd07a2eeb90c277655a4aa69
princehabeeb <habiy3400@gmail.com>
2023-07-22 17:18:00 +0100

    new

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,7 +7,10 @@ const Feed = () => {
       <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d',
       px:{sx: 0, md: 2}
       }}>
-
+      sidebar
+      <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
+        Copyright 2023 Tubio
+      </Typography>
       </Box>
     </Stack>
   )
