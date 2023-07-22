29c9eb95b0f2ae48de3d036150848bd93dc0b6a9
princehabeeb <habiy3400@gmail.com>
2023-07-22 17:06:50 +0100

    new

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,7 +4,9 @@ import { Box, Stack, Typography } from '@mui/material';
 const Feed = () => {
   return (
     <Stack sx={{flexDirection:{sx: 'column', md: 'row'}}}>
-      <Box sx={{height: {sx: 'auto', md: '92vh'}}}>
+      <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d',
+      px:{sx: 0, md: 2}
+      }}>
 
       </Box>
     </Stack>
