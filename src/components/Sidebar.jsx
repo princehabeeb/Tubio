29c334a695395dbbc34842da82c02c235e81d4e4
princehabeeb <habiy3400@gmail.com>
2023-07-23 22:21:47 +0100

    new

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,9 @@ import { Stack } from '@mui/material';
 import { categories }from '../utils/constants';
 // import { Category } from '@mui/icons-material';
 
+
+const selectedCategory = 'New';
+
 const Sidebar = () => {
   return (
     <Stack 
@@ -12,7 +15,13 @@ const Sidebar = () => {
       flexDirection: { md: 'column'}, 
   }}>
     {categories.map((category) => (
-      <button>
+      <button
+      className='category-btn'
+      style={{
+        background: category.name === selectedCategory && '#FC1503', color: 'white'
+      }}
+      key= {category.name}
+      >
         <span>{category.icon}</span>
         <span>{category.name}</span>
       </button>
