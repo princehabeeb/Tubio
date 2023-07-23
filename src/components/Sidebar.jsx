267a859f9022b6470e5e3b61463b4c5bbb8d13f3
princehabeeb <habiy3400@gmail.com>
2023-07-23 21:35:39 +0100

    new

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Stack } from '@mui/material';
+import categories from '../utils/constants';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <Stack direction='row' sx={{overflowY: 'auto',
+    height: {sx: 'auto', md: '95%'}
+    }}>
+
+    </Stack>
   )
 }
 
