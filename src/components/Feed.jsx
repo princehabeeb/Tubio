5cd0c42b215e01afaa52bcf619e135c06c2cc06c
princehabeeb <habiy3400@gmail.com>
2023-07-24 16:10:47 +0100

    new commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,6 +13,11 @@ const Feed = () => {
         Copyright 2023 Tubio
       </Typography>
       </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' mb={2} sx={{color: 'white'}}>
+         New <span style={{color: '#f31503'}}>Videos</span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
