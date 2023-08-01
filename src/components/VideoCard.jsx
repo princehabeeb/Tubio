84c339c4edf4e74c702b3c295213fa315bbdae0a
princehabeeb <habiy3400@gmail.com>
2023-08-01 18:49:18 +0100

    new

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle} from '@mui/icons-material'
+import { demoThumbnailUrl, demoChannelTitle, demoChannelUrl, demoVideoTitle, demoVideoUrl } from '../utils/constants'
 
 const VideoCard = () => {
     return (
