2d8b1fd8ecae408dd278a3ef3c3d925b223678f6
princehabeeb <habiy3400@gmail.com>
2023-08-08 15:24:13 +0100

    new

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 const ChannelDetail = () => {
   return (
