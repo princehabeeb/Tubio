31f393817c20af4561747aedb14d656e8997c2b0
princehabeeb <habiy3400@gmail.com>
2023-08-08 15:31:05 +0100

    new

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { Videos, ChannelCard } from './'
 
 const ChannelDetail = () => {
+const { id } = useParams();
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
