3c9b4cf904eeaf27aa8531638d19e639f0f9acc1
princehabeeb <habiy3400@gmail.com>
2023-08-10 11:06:49 +0100

    new

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -20,7 +20,11 @@ useEffect(() => {
 
   return (
     <Box minHeight='95vh'>
-
+       <Box>
+        <div style={{background: 
+          'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
+          zIndex: 10}} />
+       </Box>
     </Box>
   )
 }
