177a68077eccbdeae6c7226a6e987d68e2af65fa
princehabeeb <habiy3400@gmail.com>
2023-08-20 13:45:48 +0100

    new

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -16,27 +16,12 @@ const Feed = () => {
 
 
   return (
-    <Stack sx={{flexDirection:{sx: 'column', md: 'row'}}}>
-      <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d',
-      px:{sx: 0, md: 2}
-      }}>
-      <Sidebar
-      selectedCategory=
-      {selectedCategory}
-      setSelectedCategory=
-      {setSelectedCategory}
-      />
-      <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
-        Copyright 2023 Tubio
-      </Typography>
-      </Box>
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
-        <Typography variant='h4' mb={2} sx={{color: 'white'}}>
-        {selectedCategory}<span style={{color: '#f31503'}}>Videos</span>
-        </Typography>
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+    <Typography variant='h4' mb={2} sx={{color: 'white'}}>
+    {selectedCategory}<span style={{color: '#f31503'}}>Videos</span>
+    </Typography>
+    <Videos videos={videos} />
+  </Box>
   )
 }
 
