2fd51d9586c648d2cc96aacd7c92c8600822d6c9
princehabeeb <habiy3400@gmail.com>
2023-08-28 14:51:55 +0100

    new

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,23 +2,25 @@ import { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
 import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { useParams } from 'react-router-dom';
 
 
 
 const SearchFeed = () => {
 
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams()
 
   useEffect(()=> {
-     fetchFromAPI(`search?part=snippet&q=${}`)
+     fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
      .then((data) => setVideos(data.items))
-  }, []);
+  }, [searchTerm]);
 
 
   return (
     <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
     <Typography variant='h4' mb={2} sx={{color: 'white'}}>
-    {selectedCategory}<span style={{color: '#f31503'}}>Videos</span>
+    Search Results for:<span style={{color: '#f31503'}}>Videos</span>
     </Typography>
     <Videos videos={videos} />
   </Box>
