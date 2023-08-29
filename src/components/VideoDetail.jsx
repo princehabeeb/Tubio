86c8dade806ab5c9799f0de12e27abddcd34e735
princehabeeb <habiy3400@gmail.com>
2023-08-29 15:11:20 +0100

    new

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,10 +4,13 @@ import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import { Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+
+    </Box>
   )
 }
 
