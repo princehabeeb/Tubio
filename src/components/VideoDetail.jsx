4e276bc11ea376aa4dc331f0dbfa2868cca4338e
princehabeeb <habiy3400@gmail.com>
2023-08-29 14:59:54 +0100

    new

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Videos } from './';
 
 const VideoDetail = () => {
   return (
